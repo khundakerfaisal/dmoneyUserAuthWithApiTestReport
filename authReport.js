90415bec66678fd8ab7bece01c71e3f8934d859a
khundaker Faisal <khundakermis@gmail.com>
2023-12-01 01:45:04 +0600

    user auth updated with test report

---
+++ authReport.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/17475087-439cd858-22cd-479c-a13f-45c3fc5d3e45?access_key=${process.env.accessKey}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
